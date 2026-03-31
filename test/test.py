c3ca64355f76b33cbc9a3fc9ca644a53953cb86c
KangMinwoo-0719 <gangminu1122@gmail.com>
2026-03-31 23:13:01 +0900

    kNN regression study

---
+++ test/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+list_a = np.array([1, 2, 3, 4, 5, 6])
+list_b = np.array([7, 8, 9, 10, 11, 12])
+
+
+
+# test_list = [[l, w] for l, w in zip(list_a, list_b)]
+
+# output_list = np.array(test_list)
+
+# print(output_list)
